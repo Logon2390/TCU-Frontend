23f43fe5cd1991e549a3a5f1649d74791216128e
Logon2390 <jorgeluis7040@gmail.com>
2025-04-12 18:35:26 -0600

    Add UserLoginView component to router and simplify route definition

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import UserLoginView from '../views/user/LoginView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,14 +11,9 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue'),
-    },
-    {
       path: '/user/Login',
       name: 'user',
-      component: () => import('../views/user/LoginView.vue'),
+      component: UserLoginView,
     },
   ],
 })
