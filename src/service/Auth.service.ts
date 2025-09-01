02d2493fc0e5360743d9c72b64f8b759f0f3bb83
Logon2390 <jorgeluis7040@gmail.com>
2025-08-31 19:51:45 -0600

    Implement user authentication features in AppHeader and LoginView
    - Updated AppHeader.vue to conditionally display sidebar and session controls based on user authentication status.
    - Added logout functionality with confirmation modal in AppHeader.
    - Removed 'Iniciar sesión' link from header configuration as it is now handled in LoginView.
    - Enhanced LoginView.vue to support a verification phase after login, allowing users to enter a verification code sent to their email.
    - Integrated loading states for login and verification processes to improve user experience.

--- src/service/Auth.service.ts
+++ src/service/Auth.service.ts
@@ -7,9 +7,24 @@ export default {
     try {
       const response = await api.post(`${AUTH_URL}/login`, { email, password })
       const data = response.data
-      if (data?.token) {
-        localStorage.setItem('authToken', data.token)
-      }
+      return data
+    } catch (error) {
+      console.error(error)
+    }
+  },
+  logout: async () => {
+    try {
+      const response = await api.get(`${AUTH_URL}/logout`)
+      const data = response.data
+      return data
+    } catch (error) {
+      console.error(error)
+    }
+  },
+  verifyCode: async (email: string, accessCode: string) => {
+    try {
+      const response = await api.post(`${AUTH_URL}/verifyAccessCode`, { email, accessCode })
+      const data = response.data
       return data
     } catch (error) {
       console.error(error)
