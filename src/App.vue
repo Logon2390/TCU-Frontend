2245ca26be5148fe544732940997c212a3f85222
Logon2390 <jorgeluis7040@gmail.com>
2025-09-11 14:02:24 -0600

    Add accessibility features to App.vue and update dependencies
    - Integrated the 'accessibility' library into App.vue to enhance user experience with accessibility options.
    - Added configuration for accessibility features including text resizing, color inversion, and text-to-speech.
    - Updated package.json and package-lock.json to include 'accessibility' and its dependencies.

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,52 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import { Accessibility, type IAccessibilityIconPositionOptions } from 'accessibility';
 import Header from './components/layout/AppHeader.vue'
 import Footer from './components/layout/AppFooter.vue'
 import Sidebar from './components/layout/AppSidebar.vue'
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  let accessibility = new Accessibility({
+    labels: {
+      resetTitle: "Reiniciar",
+      closeTitle: "Cerrar",
+      increaseText: "Aumentar texto",
+      decreaseText: "Disminuir texto",
+      increaseTextSpacing: "Aumentar espaciado",
+      decreaseTextSpacing: "Disminuir espaciado",
+      invertColors: "Invertir colores",
+      grayHues: "Grises",
+      menuTitle: "Menú",
+      textToSpeech: "Texto a voz",
+      bigCursor: "Cursor grande",
+      readingGuide: "Guía de lectura",
+      underlineLinks: "Subrayar enlaces",
+      speechToText: "Texto a voz",
+      disableAnimations: "Deshabilitar animaciones",
+      increaseLineHeight: "Aumentar altura",
+      decreaseLineHeight: "Disminuir altura",
+      hotkeyPrefix: "Prefijo de teclado",
+    },
+    modules: {
+      increaseText: true,
+      decreaseText: true,
+      increaseTextSpacing: true,
+      decreaseTextSpacing: true,
+      increaseLineHeight: false,
+      decreaseLineHeight: false,
+      readingGuide: false,
+      underlineLinks: true,
+      speechToText: true,
+      disableAnimations: true,
+      textToSpeech: true,
+      bigCursor: false,
+      invertColors: true,
+      grayHues: true,
+    },
+    textToSpeechLang: 'es-ES',
+  });
+});
 </script>
 
 <template>
